cadfa050090710c158414051b55d23807740ac2b
MAlbino21 <102793083+MAlbino21@users.noreply.github.com>
2022-04-05 15:53:42 +0800

    Add files via upload

---
+++ conn.php
@@ -0,0 +1,8 @@
+<?php
+	//mysqsll_connect(server, username, password, database)
+	$conn = mysqli_connect("localhost", "root", "", "db_grade");
+	
+	if(!$conn){
+		die("Error: Failed to connect to database!");
+	}
+?>
